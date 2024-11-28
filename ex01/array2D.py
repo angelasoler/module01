827cfcf755cb31d4207ffe2d405945322066be65
angelasoler <manzanaembicicleta@gmail.com>
2024-11-27 21:17:27 -0300

    ex01 doc

--- ex01/array2D.py
+++ ex01/array2D.py
@@ -3,6 +3,14 @@ import sys
 
 
 def validate_args(lst) -> np.array:
+    """validate slice_me funct arguments
+
+    Args:
+        lst (list): de argument to be validated
+
+    Returns:
+        np.array: the array from lst if validations passed, if not exit
+    """
     try:
         assert isinstance(lst, list), "family param most be list type"
         array = np.array(lst)
@@ -14,6 +22,18 @@ def validate_args(lst) -> np.array:
 
 
 def slice_me(family: list, start: int, end: int) -> list:
+    """ truncates the input 2D array by slicing rows based
+    on the given indices (start and end) and recalculates
+    the shape after slicing
+
+    Args:
+        family (list): 2D list
+        start (int): index
+        end (int): index
+
+    Returns:
+        list: the list resulting from the trunc
+    """
     array = validate_args(family)
 
     print(f"My shape is : {array.shape}")
